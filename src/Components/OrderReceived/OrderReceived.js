579aae22ed1a5650f9b0b0df2aedf430990f23d7
Golufem <oluwafemigbadero@gmail.com>
2022-10-19 11:53:00 +0100

    seventh commit

--- src/Components/OrderReceived/OrderReceived.js
+++ src/Components/OrderReceived/OrderReceived.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Modal from '../UI/Modal/Modal';
+import styles from './OrderReceivedStyle.module.css'
 
 
 
 const OrderReceived = (props) => {
   return (
     <Modal>
-      <h1>Order Successfully Recevied</h1>
-      <button onClick={props.onHideOrder}>Ok</button>
+     <div className={styles.wrap}>
+     <h1 className={styles.text}>Order Successfully Recevied</h1>
+     <button onClick={props.onHideOrder}className = {styles.button}>Ok</button>
+     </div>
     </Modal>
   );
 }
